86e1de5c71749c1f818c4af602fbcd36ed56a971
1409394 <piers.addison@gmail.com>
2016-10-03 15:55:59 +0100

    PHP Section 3

--- index.php
+++ index.php
@@ -8,11 +8,15 @@
         <p>
             <?php
             echo "Hello World";
+            echo "<br>";
             echo "Hello," . " " . "world" . "!";
+            echo "<br>";
             echo 5 * 7;
-            $myName = "Bilbo Baggins";
-            $myAge = 111;
-            echo "My name is " . $myName . " and I am " . $myAge;
+            echo "<br>";
+            $hobbitName = "Bilbo Baggins";
+            $hobbitAge = 111;
+            echo "My name is " . $hobbitName . " and I am " . $hobbitAge;
+            echo "<br>";
             echo "I get printed";
 
             // I don't get printed.
@@ -38,7 +42,51 @@
                 print "Who are you?";
             }
 
+            echo "<br>";
 
+            $myName = "";
+            $myAge = 0;
+            $allowedItems = "";
+
+            if($myAge < 16)
+            {
+                $allowedItems = "You cannot buy any items.";
+            }
+            else if($myAge < 18)
+            {
+                $allowedItems = "You may buy specs.";
+            }
+            else if($myAge < 21)
+            {
+                $allowedItems = "You may buy specs and mugs.";
+            }
+            else
+            {
+                $allowedItems = "You may buy sepcs, mugs and sausage rolls.";
+            }
+
+            echo "My name is " . $myName . ". I am " . $myAge . ". " . $allowedItems;
+
+            echo "<br>";
+
+            $wantedGood = "mug";
+
+            switch($wantedGood)
+            {
+                case "specs":
+                    echo "you must be 16 or over to purchase specs.";
+                    break;
+                case "mugs":
+                    echo "You must be 18 or over to purchase mugs.";
+                    break;
+                case "sausage rolls":
+                    echo "You must be 21 or over to purchase sausage rolls.";
+                    break;
+                default:
+                    echo "Please enter a valid item";
+            }
+
+            echo "<br>";
             ?>
         </p>
     </body>
